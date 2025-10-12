38d490b63b8f80163645004b30c2e3f1f8d97e97
taejeong <kungseman0509@gmail.com>
2025-10-12 17:42:27 +0900

    초기파일

---
+++ src/test/java/com/example/GreenCloset/GreenClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenCloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenClosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
