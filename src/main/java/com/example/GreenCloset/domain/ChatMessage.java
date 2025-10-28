843f074d93427240354802b1f1536d340e9be72b
taejeong <kungseman0509@gmail.com>
2025-10-28 15:20:16 +0900

    domain 설정

--- src/main/java/com/example/GreenCloset/domain/ChatMessage.java
+++ src/main/java/com/example/GreenCloset/domain/ChatMessage.java
@@ -1,4 +1,39 @@
 package com.example.GreenCloset.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Chat_Messages")
+@Getter
+@NoArgsConstructor
+
 public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Long id;
+
+    @Column(name = "sender_id")
+    private Long senderId;
+
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(name = "sender_id")
+    private Long SenderId;
+
+    @Column(name = "content",length = 500)
+    private String content;
+
+    @Column(name = "sent_at",nullable = false)
+    private LocalDateTime sentAt;
+
 }
+
+
+sender_id   room_id    sender_id    content sent_at
--- src/main/java/com/example/GreenCloset/domain/ChatRoom.java
+++ src/main/java/com/example/GreenCloset/domain/ChatRoom.java
@@ -1,4 +1,29 @@
 package com.example.GreenCloset.domain;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Chat_Rooms")
+@Getter
+@NoArgsConstructor
+
 public class ChatRoom {
+    @Id
+    @Column(name = "room_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "buyer_id")
+    private Long buyerId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
 }
